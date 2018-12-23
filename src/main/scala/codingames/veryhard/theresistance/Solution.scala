ded74fce62fa62fc913f35c24061770ea9f43da4
JolyRoger <mjelnr@gmail.com>
2018-12-23 20:08:42 +0100

    map

--- src/main/scala/codingames/veryhard/theresistance/Solution.scala
+++ src/main/scala/codingames/veryhard/theresistance/Solution.scala
@@ -1,8 +1,11 @@
 package codingames.veryhard.theresistance
 
+import java.util.regex.{Matcher, Pattern}
+
 object Solution extends App {
 
   val wordMap: scala.collection.mutable.Map[String, Int] = scala.collection.mutable.Map.empty[String, Int]
+  val posStartMap: scala.collection.mutable.Map[Int, Map[Int, Int]] = scala.collection.mutable.Map.empty[Int, Map[Int, Int]]
 
   def morze: Map[String, Char] = Map(
     ".-" ->   'A', "-..." -> 'B', "-.-." -> 'C', "-.." ->  'D',
@@ -17,29 +20,32 @@ object Solution extends App {
 
   def toMorze(word: String) = word.foldLeft("")(_ + morze2(_))
 
-  def wordPair(word: String) = {
-    val mword = toMorze(word)
-    if (wordMap.contains(mword)) {
-      wordMap.put(mword, wordMap(mword) + 1)
-    } else {
-      wordMap.put(mword, 1)
+  def wordPair(word: String, seq: String) = {
+    val rmword = toMorze(word).replace(".", "\\.")
+
+    val pattern: Pattern = Pattern.compile(rmword)
+    val matcher: Matcher = pattern.matcher(seq)
+
+    while (matcher.find) {
+      val start = matcher.start
+      val end = matcher.end
+
+      posStartMap.get(start) match {
+        case None => posStartMap.put(start, Map(end -> 1))
+        case Some(wordInfo) => posStartMap.put(start, wordInfo + (end -> (wordInfo.getOrElse(end, 0) + 1)))
+      }
     }
   }
 
-  def find(seq: Stream[Char]): Int = {
-    if (seq.isEmpty) 1 else {
-      val res = for {
-        indexedSeq <- seq.zipWithIndex
-        candidate = seq.take(indexedSeq._2 + 1).foldLeft("")(_ + _)
-        if wordMap.contains(candidate)
-      } yield wordMap(candidate) * find(seq.drop(indexedSeq._2 + 1))
-      res.sum
+  def find(seq: String, index: Int): Int = {
+    posStartMap.get(index) match {
+      case Some(infoMap) => (for ((end, multiplier) <- infoMap) yield multiplier * find(seq.drop(end), end)).sum
+      case None => if (seq.isEmpty) 1 else 0
     }
   }
 
-  val seq = readLine.toStream
+  val seq = readLine
   val dictSize = readInt
-  for (i <- 0 until dictSize) wordPair(readLine)
-
-  println(find(seq))
+  for (i <- 0 until dictSize) wordPair(readLine, seq)
+  println(find(seq, 0))
 }
