a5eb5265e7d7507f9fa070c17e93304496097590
JolyRoger <mjelnr@gmail.com>
2018-12-30 03:02:40 +0100

    works

--- src/main/scala/codingames/veryhard/theresistance/Solution.scala
+++ src/main/scala/codingames/veryhard/theresistance/Solution.scala
@@ -1,14 +1,12 @@
-//package codingames.veryhard.theresistance
+package codingames.veryhard.theresistance
 
-import java.util.regex.{Matcher, Pattern}
-import scala.collection.mutable
+import java.util.regex.Pattern
 
 object Solution extends App {
 
-  val wordMap: scala.collection.mutable.Map[String, Int] = scala.collection.mutable.Map.empty[String, Int]
-  val posStartMap: scala.collection.mutable.Map[Int, Map[Int, Int]] = scala.collection.mutable.Map.empty[Int, Map[Int, Int]]
+  val posStartMap = scala.collection.mutable.Map.empty[Int, Map[Int, Int]]
 
-  val morze: Map[String, Char] = Map(
+  def morze: Map[String, Char] = Map(
     ".-" ->   'A', "-..." -> 'B', "-.-." -> 'C', "-.." ->  'D',
     "." ->    'E', "..-." -> 'F', "--." ->  'G', "...." -> 'H',
     ".." ->   'I', ".---" -> 'J', "-.-" ->  'K', ".-.." -> 'L',
@@ -17,15 +15,12 @@ object Solution extends App {
     "..-" ->  'U', "...-" -> 'V', ".--" ->  'W', "-..-" -> 'X',
     "-.--" -> 'Y', "--.." -> 'Z')
 
-  val morze2 = morze.map(_.swap)
+  def morzeReverse = morze.map(_.swap)
 
-  def toMorze(word: String) = word.foldLeft("")(_ + morze2(_))
+  def toMorze(word: String) = word.foldLeft("")(_ + morzeReverse(_))
 
   def wordPair(word: String, seq: String) = {
-    val rmword = toMorze(word).replace(".", "\\.")
-
-    val pattern: Pattern = Pattern.compile(rmword)
-    val matcher: Matcher = pattern.matcher(seq)
+    val matcher = Pattern.compile(toMorze(word).replace(".", "\\.")).matcher(seq)
 
     while (matcher.find) {
       val start = matcher.start
@@ -38,30 +33,19 @@ object Solution extends App {
     }
   }
 
-  def allPaths(from: Int, to: Int): Long = {
-    val stack = mutable.Stack[(Int, Int)]()
-    stack.push(from -> 1)
-    var mul = 1
-    var sum: Long = 0
+  def find(seq: String) = {
+    val pathArray = Array.fill[Long](seq.length + 1)(0)
+    pathArray(pathArray.length - 1) = 1
 
-    while (stack.nonEmpty) {
-      val (newIndex, newMul) = stack.pop
-      mul = mul * newMul
-      sum = posStartMap.get(newIndex) match {
-        case None =>
-          val nm = mul
-          mul = 1
-          if (newIndex == to) sum + nm else sum
-        case Some(info) =>
-          stack.pushAll(info)
-          sum
-      }
-    }
-    sum
+    posStartMap.keys.toList.sortWith(_ > _).foreach(key => {
+      val info = posStartMap(key)
+      pathArray(key) = info.keys.toList.map(innerKey => pathArray(innerKey) * info(innerKey)).sum
+    })
+    pathArray(0)
   }
 
   val seq = readLine
   val dictSize = readInt
   for (i <- 0 until dictSize) wordPair(readLine, seq)
-  println(allPaths(0, seq.length))
+  println(find(seq))
 }
