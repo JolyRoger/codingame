f04c29880548ebd482d6bedb66de1bebac6d73cc
JolyRoger <mjelnr@gmail.com>
2019-02-11 11:46:55 +0100

    mars lander easy

--- src/main/scala/codingames/easy/marslander/Player.scala
+++ src/main/scala/codingames/easy/marslander/Player.scala
@@ -1,4 +1,4 @@
-//package codingames.easy.marslander
+package codingames.easy.marslander
 
 import math._
 import scala.util._
@@ -18,7 +18,7 @@ object Player extends App {
 //  data.foreach(c => Console.err.println(s"x=${c.mkString("[", ":", "]")}"))
 //  dataReverse.foreach(c => Console.err.println(s"xRev=${c.mkString("[", ":", "]")}"))
   //    data.takeWhile()
-  def gravity = 3.711f
+  def g = 3.711f
   def powers = List.range(0, 5)
   def nextPowers(currentPower: Int) = if (currentPower == 0) List(0, 1) else if (currentPower == 4) List(3, 4)
     else List(currentPower - 1, currentPower, currentPower + 1)
@@ -26,16 +26,40 @@ object Player extends App {
       if (entered > current) current + 1 else current - 1
     } else entered
 
+  def timeOfFall(h: Int, y: Int, g: Float, thrust: Int)= sqrt(2 * (h - y) / (g - thrust))
+  def speedInPoint(h: Int, g: Float, y: Int, thrust: Int)=  sqrt(2 * (h - y) * (g - thrust))
+  def speedOfGround(h: Int, g: Float, thrust: Int)= speedInPoint(h, g, 100, thrust)
+  def afterSec(h: Int, g: Float, t: Int)= h - g * t * t / 2
 
-  def findThrustPower(ground: Int, y: Int, vspeed: Int, currentFuel: Long, currentPower: Int) = {
+  def findThrustPower(ground: Int, y: Int, vspeed: Int, currentFuel: Int, currentPower: Int) = {
     def calculateNextState(power: Int) = {
       val newFuel = currentFuel - power
-      val newVspeed = Math.round(vspeed - gravity + power)
-      val newY = y + newVspeed
+//      val newVspeed = round(vspeed - g + power)
+      val newVspeed = round(vspeed - (g - power))
+      val newY = round(y + (vspeed - (g - power) / 2))
       (newVspeed, newY, newFuel)
     }
     Console.err.println(s"vspeed=$vspeed y=$y fuel=$currentFuel power=$currentPower")
     calculateNextState(currentPower)
+  }
+
+  def calcNext(y: Int, vspeed: Int, fuel: Int, power: Int) = {
+    val newFuel = fuel - power
+    val newVspeed = round(vspeed - (g - power))
+    val newY = round(y + (vspeed - (g - power) / 2))
+    Console.err.println(s"DATA: y=$y power=$power\t${(newVspeed, newY, newFuel, power)}")
+    (newVspeed, newY, newFuel, power)
+  }
+
+  // сколько топлива надо потратить при данных условиях чтобы достичь земли
+  def findFuel(ground: Int, vspeed: Int, y: Int, fuel: Int, power: Int, acc: Int): Int = {
+    if (y <= ground) if (vspeed < -40) 1000 else power else {
+      val powers = nextPowers(power)
+      val nextStates = powers.map(p => calcNext(y, vspeed, fuel, p)).filter(_._2 < y)
+      val minNextState = nextStates.minBy(state => state._4 + findFuel(ground, state._1, state._2, state._3, state._4, acc))
+      Console.err.print(s"$power+${minNextState._4} ")
+      acc + power + minNextState._4
+    }
   }
 
   var i = 0
@@ -46,9 +70,11 @@ object Player extends App {
     if (vspeed < -40) flag = false
     val tp = if (flag) 0 else if (vspeed < -39) 4 else 3
     val yLevel = dataReverse.dropWhile(coord => coord(0) > x).head(1)
-    val (newSpeed, newY, newFuel) = findThrustPower(yLevel, y, vspeed, fuel, findNextPower(tp, power))
+    val (newSpeed, newY, newFuel) = findThrustPower(yLevel, y, vspeed, fuel, findNextPower(/*tp*/3, power))
     Console.err.println(s"newspeed=$newSpeed newY=$newY newfuel=$newFuel")
-    println("0 " + tp/* + (i % 5)*/)
+//    println("0 " + tp/* + (i % 5)*/)
+    Console.err.println(s"$i sec passed")
+    println("0 " + 3)
     i = i + 1
   }
 }
--- src/test/scala/codingame/easy/marslander/PlayerTests.scala
+++ src/test/scala/codingame/easy/marslander/PlayerTests.scala
@@ -4,7 +4,32 @@ import codingames.easy.marslander.Player._
 import org.scalatest.FlatSpec
 
 class PlayerTests extends FlatSpec {
+
   "A player" should "find thrust power" in {
-    findThrustPower(100,  2500, 0, 5001, 0)
+    val gEarth = 9.81523f
+    val thrust = 0
+    val thrustRange = 0 to 4
+
+    for (thr <- thrustRange) {
+      Console.err.println
+  //    findThrustPower(100,  2500, 0, 5001, 0)
+      val timeMars = timeOfFall(2500, 100, g, thr)
+      val speedMars = speedOfGround(2500, g, thr)
+      val timeEarth = timeOfFall(2500, 100, gEarth, thr)
+      val speedEarth = speedOfGround(2500, gEarth, thr)
+      Console.err.println(s"Mars:  time of fall = $timeMars\tspeed = $speedMars\tthrust = $thr")
+      Console.err.println(s"Earth: time of fall = $timeEarth\tspeed = $speedEarth\tthrust = $thr")
+    }
+    Console.err.println
+
+    for (t <- 0 to 36) {
+      val dist = afterSec(2500, g, t)
+      Console.err.println(s"after $t sec in $dist point")
+    }
+  }
+
+  "A player" should "find simple solution" in {
+    val res = findFuel(100, -37, 136, 500, 0, 0)
+    Console.err.println(s"FUEL: $res")
   }
 }
