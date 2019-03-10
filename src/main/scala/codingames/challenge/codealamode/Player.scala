bd00c37538914b2bdc8a811403228c3eb2cb9955
JolyRoger <mjelnr@gmail.com>
2019-03-10 21:17:34 +0100

    works

--- src/main/scala/codingames/challenge/codealamode/Player.scala
+++ src/main/scala/codingames/challenge/codealamode/Player.scala
@@ -1,83 +1,115 @@
+//package codingames.challenge.codealamode
+
+import scala.collection.mutable
 
 object MyState extends Enumeration {
   type Margin = Value
   val EMPTY, WITH_EMPTY_PLATE, WITH_BLUEBERRY_PLATE, WITH_ICECREAM_PLATE, WITH_BOTH_PLATE = Value
 }
 
-object Player extends App {
-  val numallcustomers = readInt
-  Console.err.println(s"numallcustomers: $numallcustomers")
-  for (i <- 0 until numallcustomers) {
-    // customeritem: the food the customer is waiting for
-    // customeraward: the number of points awarded for delivering the food
-    val Array(customeritem, _customeraward) = readLine split " "
-    val customeraward = _customeraward.toInt
-//    Console.err.println(s"\tcustomeritem: $customeritem customeraward: $customeraward")
+class Graph {
+  val N = 7 * 11
+  val adj = (for (i <- 0 until N) yield List[Int]()).toArray
+  def addEdge(v: Int, w: Int){
+    adj(v) = w :: adj(v)
+    adj(w) = v :: adj(w)
   }
-  var dishes: (Int, Int) = (-1, -1)
-  var window: (Int, Int) = (-1, -1)
-  var blueberry: (Int, Int) = (-1, -1)
-  var icecream: (Int, Int) = (-1, -1)
-  var myState = MyState.EMPTY
 
-  for (i <- 0 until 7) {
-    val kitchenline = readLine
-    Console.err.println(s"\t\tkitchenline[$i]: $kitchenline")
-    if (kitchenline.contains("D")) dishes = (kitchenline.indexOf('D'), i)
-    if (kitchenline.contains("W")) window = (kitchenline.indexOf('W'), i)
-    if (kitchenline.contains("B")) blueberry = (kitchenline.indexOf('B'), i)
-    if (kitchenline.contains("I")) icecream = (kitchenline.indexOf('I'), i)
+  def bfs(s: Int) = {
+    val marked: Array[Boolean] = new Array[Boolean](N)
+    val edgeTo = Array.fill[Int](N)(Int.MaxValue)
+    val distTo = Array.fill[Int](N)(Int.MaxValue)
+    val q = mutable.Queue[Int]()
+    var i = 0
+
+    q.enqueue(s)
+    marked(s) = true
+    distTo(s) = 0
+    while (q.nonEmpty) {
+      val v = q.dequeue
+      i = i + 1
+      adj(v).filterNot(marked).foreach(
+        w => {
+          q.enqueue(w)
+          marked(w) = true
+          edgeTo(w) = v
+          distTo(w) = distTo(v) + 1
+        }
+      )
+    }
+    (edgeTo, distTo)
   }
+}
+
+object Player extends App {
+  type Point = (Int, Int)
+  implicit def toNumber(point: Point): Int = point._2 * 11 + point._1 % 11
+  def toMatrix(number: Int): Point = (number % 11, number / 11)
+  def walk(matrix: List[String], row: Int, col: Int) = row > 0 && row < 7 && col > 0 && col < 11 && (matrix(row)(col) == '.' || matrix(row)(col) == '0')
+  def replaceSym(matrix: List[String], oldSym: Char, newSym: Char) = matrix.map(_.replace(oldSym, newSym))
+  def searchSym(matrix: List[String], sym: Char) = {
+    val coords = for (row <- matrix.indices; col <- matrix(row).indices; _sym = matrix(row)(col); if _sym == sym) yield (col, row)
+    if (coords.nonEmpty) Some(coords(0)) else None
+  }
+  def adjTo(matrix: List[String], point: Point): List[Point] = List((point._1 - 1, point._2 - 1), (point._1, point._2 - 1),
+                   (point._1 + 1, point._2 - 1), (point._1 - 1, point._2), (point._1 + 1, point._2), (point._1 - 1, point._2 + 1),
+                   (point._1, point._2 + 1), (point._1 + 1, point._2 + 1)).filter(p => walk(matrix, p._2, p._1))
+  def nextTarget(playerItem: String, customerItems: List[String]): List[String] = {
+      def same(component: String) = customerItems.map(_.split('-')(1)).distinct.contains(component)
+      val orderMap = Map("NONE" -> List("DISH"),
+        "DISH" -> List("BLUEBERRIES", "ICE_CREAM").filter(same),
+        "ICE_CREAM" -> List("BLUEBERRIES"),
+        "DISH-ICE_CREAM" -> List("BLUEBERRIES"),
+        "DISH-BLUEBERRIES" -> List("ICE_CREAM"),
+        "DISH-ICE_CREAM-BLUEBERRIES" -> List("WINDOW"),
+        "DISH-BLUEBERRIES-ICE_CREAM" -> List("WINDOW")
+      )
+      orderMap(playerItem)
+    }
+
+  def createGraph(matrix: List[String]) = {
+    val g = new Graph
+    for (row <- matrix.indices; col <- matrix(row).indices; sym = matrix(row)(col); if walk(matrix, row, col)) {
+      if (walk(matrix, row - 1, col)) g.addEdge((col, row), (col, row - 1))
+      if (walk(matrix, row, col - 1)) g.addEdge((col, row), (col - 1, row))
+    }
+    g
+  }
+
+  def reach(g: Graph, matrix: List[String], player: Point, target: List[Point]) = s"USE ${target.head._1} ${target.head._2}"
+
+  val numallcustomers = readInt
+  val customerData = (for (i <- 0 until numallcustomers) yield readLine split " ").toList
+  val graphMatrix = (for (i <- 0 until 7) yield readLine).toList
+  val targetMap = Map("DISH" -> searchSym(graphMatrix, 'D').getOrElse((-1,-1)),
+                      "ICE_CREAM" -> searchSym(graphMatrix, 'I').getOrElse((-1,-1)),
+                      "BLUEBERRIES" -> searchSym(graphMatrix, 'B').getOrElse((-1,-1)),
+                      "WINDOW" -> searchSym(graphMatrix, 'W').getOrElse((-1,-1)))
 
   // game loop
   while (true) {
     val turnsremaining = readInt
-    Console.err.println(s"turnsremaining: $turnsremaining")
     val Array(_playerx, _playery, playeritem) = readLine split " "
     val playerx = _playerx.toInt
     val playery = _playery.toInt
-    Console.err.println(s"playerx: $playerx, playery: $playery, playeritem: $playeritem")
     val Array(_partnerx, _partnery, partneritem) = readLine split " "
     val partnerx = _partnerx.toInt
     val partnery = _partnery.toInt
-    Console.err.println(s"partnerx: $partnerx, partnery: $partnery, partneritem: $partneritem")
-    val numtableswithitems = readInt // the number of tables in the kitchen that currently hold an item
-    Console.err.println(s"numtableswithitems: $numtableswithitems")
+    val numtableswithitems = readInt
     for (i <- 0 until numtableswithitems) {
       val Array(_tablex, _tabley, item) = readLine split " "
       val tablex = _tablex.toInt
       val tabley = _tabley.toInt
-      Console.err.println(s"\ttablex: $tablex, tabley: $tabley")
     }
-    // ovencontents: ignore until wood 1 league
     val Array(ovencontents, _oventimer) = readLine split " "
     val oventimer = _oventimer.toInt
-    Console.err.println(s"ovencontents: $ovencontents, oventimer: $oventimer")
-    val numcustomers = readInt // the number of customers currently waiting for food
-    Console.err.println(s"numcustomers: $numcustomers")
-    val customers = (for (i <- 0 until numcustomers) yield readLine split " ").sortBy(_(0))
-    customers.foreach(c => Console.err.println(s"customeritem: ${c(0)}, customeraward: ${c(1)}"))
-//    for (i <- 0 until numcustomers) {
-//      val Array(customeritem, _customeraward) = readLine split " "
-//      val customeraward = _customeraward.toInt
-//      Console.err.println(s"\tcustomeritem: $customeritem, customeraward: $customeraward")
-//    }
+    val numcustomers = readInt
+    val customers = (for (i <- 0 until numcustomers) yield readLine split " ").toList.sortBy(_(0))
 
-    val action = "USE"
-    val (coord, nextState) = if (myState == MyState.EMPTY) (dishes, MyState.WITH_EMPTY_PLATE)
-                else if (myState == MyState.WITH_EMPTY_PLATE) (blueberry, MyState.WITH_BLUEBERRY_PLATE)
-                else if (myState == MyState.WITH_BLUEBERRY_PLATE) (icecream, MyState.WITH_BOTH_PLATE)
-                else if (myState == MyState.WITH_ICECREAM_PLATE) (blueberry, MyState.WITH_BOTH_PLATE)
-                else if (myState == MyState.WITH_BOTH_PLATE) (window, MyState.EMPTY) else ((-1,-1), MyState.EMPTY)
+    val g = createGraph(graphMatrix)
+    val target = nextTarget(playeritem, customers.map(_(0)))
+    val action = reach(g, graphMatrix, (playerx, playery), target.map(targetMap.apply))
 
-    // Write an action using println
-    // To debug: Console.err.println("Debug messages...")
-
-
-    // MOVE x y
-    // USE x y
-    // WAIT
-    println(s"USE ${coord._1} ${coord._2}; COORDS: ${coord._1} ${coord._2}, STATE: $myState")
-    myState = nextState
+    println(action)
   }
 }
---
+++ src/test/scala/codingame/challenge/codealamode/PlayerTests.scala
@@ -0,0 +1,78 @@
+package codingame.challenge.codealamode
+
+import codingames.challenge.codealamode.Player
+import codingames.challenge.codealamode.Player.searchSym
+import org.scalatest.FlatSpec
+
+class PlayerTests extends FlatSpec {
+  val testMatrix = List(
+    "#I###D#####",
+    "B0.1......#",
+    "#.####.##.#",
+    "#.#..#..#.#",
+    "#.##.####.#",
+    "#.........#",
+    "#####W#####"
+  )
+  val targetMap = Map("DISH" -> searchSym(testMatrix, 'D').getOrElse((-1,-1)),
+                      "ICE_CREAM" -> searchSym(testMatrix, 'W').getOrElse((-1,-1)),
+                      "BLUEBERRIES" -> searchSym(testMatrix, 'B').getOrElse((-1,-1)),
+                      "WINDOW" -> searchSym(testMatrix, 'I').getOrElse((-1,-1)))
+  
+  "A Player" should "breadth-first search" in {
+    val g = Player.createGraph(testMatrix)
+    val bfs = g.bfs(Player.toNumber((1, 1)))
+    Console.err.println(s"$bfs")
+  }
+
+  "A Player" should "convert number to matrix and back" in {
+    val point = (10, 2)
+    val square = 32
+    assert(Player.toMatrix(square) == point)
+    assert(Player.toNumber(point) == square)
+    assert(Player.toNumber(Player.toMatrix(square)) == square)
+  }
+
+  "A Player" should "find adjacent squares" in {
+    val testPoint = (5, 0)
+    val testPoint2 = (9, 3)
+    val testPoint3 = (10, 6)
+    val res = Player.adjTo(testMatrix, testPoint)
+    val res2 = Player.adjTo(testMatrix, testPoint2)
+    val res3 = Player.adjTo(testMatrix, testPoint3)
+    assert(res == List((4, 1), (5, 1), (6, 1)))
+    assert(res2 == List((9, 2), (9, 4)))
+    assert(res3 == List((9, 5)))
+  }
+  "A Player" should "find another target" in {
+    val customeritems = List("DISH-ICE_CREAM-BLUEBERRIES", "DISH-ICE_CREAM-BLUEBERRIES", "DISH-ICE_CREAM-BLUEBERRIES")
+    val playeritem = "DISH-ICE_CREAM"
+    val nextTarget = Player.nextTarget(playeritem, customeritems)
+    Console.err.println(s"nextTarget: $nextTarget")
+    assert(nextTarget == List("BLUEBERRIES"))
+  }
+
+  "A Player" should "find next target" in {
+    val customeritems = List("DISH-BLUEBERRIES-ICE_CREAM", "DISH-ICE_CREAM-BLUEBERRIES", "DISH-ICE_CREAM-BLUEBERRIES")
+    val result = List(List("DISH"), List("BLUEBERRIES"), List("ICE_CREAM"), List("WINDOW"), List("BLUEBERRIES", "ICE_CREAM"))
+    val playeritem = List("NONE", "DISH-ICE_CREAM", "DISH-BLUEBERRIES", "DISH-BLUEBERRIES-ICE_CREAM", "DISH")
+
+    val nextTarget = playeritem.map(Player.nextTarget(_, customeritems)).zipWithIndex
+    nextTarget.foreach {
+      next => {
+        Console.err.println(s"prev state: ${playeritem(next._2)}, target: ${next._1}")
+        assert(next._1 == result(next._2))
+      }
+    }
+  }
+  
+  "A Player" should "reach target" in {
+    val playeritem = "NONE"
+    val customeritems = List("DISH-BLUEBERRIES-ICE_CREAM", "DISH-ICE_CREAM-BLUEBERRIES", "DISH-ICE_CREAM-BLUEBERRIES")
+    val nextTarget = Player.nextTarget(playeritem, customeritems)
+    val g = Player.createGraph(testMatrix)
+    val player = Player.searchSym(testMatrix, '0').getOrElse((-1,-1))
+    val res = Player.reach(g, testMatrix, player, nextTarget.map(targetMap.apply))
+    Console.err.println(s"res: $res")
+  }
+}
