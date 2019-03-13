c3938e708256da3c3fa3d8e05315770ed23d24ff
JolyRoger <mjelnr@gmail.com>
2019-03-13 23:33:37 +0100

    croissant

--- src/main/scala/codingames/challenge/codealamode/Player.scala
+++ src/main/scala/codingames/challenge/codealamode/Player.scala
@@ -1,58 +1,79 @@
-package codingames.challenge.codealamode
-
-import scala.collection.mutable
-
-class Graph {
-  val N = 7 * 11
-  val adj = (for (i <- 0 until N) yield List[Int]()).toArray
-  def addEdge(v: Int, w: Int){
-    adj(v) = w :: adj(v)
-    adj(w) = v :: adj(w)
-  }
-
-  def bfs(s: Int) = {
-    val marked: Array[Boolean] = new Array[Boolean](N)
-    val edgeTo = Array.fill[Int](N)(Int.MaxValue)
-    val distTo = Array.fill[Int](N)(Int.MaxValue)
-    val q = mutable.Queue[Int]()
-    var i = 0
-
-    q.enqueue(s)
-    marked(s) = true
-    distTo(s) = 0
-    while (q.nonEmpty) {
-      val v = q.dequeue
-      i = i + 1
-      adj(v).filterNot(marked).foreach(
-        w => {
-          q.enqueue(w)
-          marked(w) = true
-          edgeTo(w) = v
-          distTo(w) = distTo(v) + 1
-        }
-      )
-    }
-    (edgeTo, distTo)
-  }
-}
-
+//package codingames.challenge.codealamode
 
 object Player extends App {
 //  case class Recipe (ingr: List[Map[String, Int]], f: List[Map[String, Int]] => List[Map[String, Int]])
-
   type Point = (Int, Int)
   type Recipe = List[Map[String, Int]]
-  val iceCreamBlueberries = List("DISH", "BLUEBERRIES", "ICE_CREAM", "WINDOW")
-  val iceCreamChoppedStrawberriesBlueberries = List("STRAWBERRIES", "CHOPPING", "DISH", "BLUEBERRIES", "WINDOW")
-  val iceCreamChoppedStrawberries = List("STRAWBERRIES", "CHOPPING", "DISH", "BLUEBERRIES", "ICE_CREAM", "WINDOW")
-  val cookBook = Map("DISH-BLUEBERRY-ICE_CREAM" -> iceCreamBlueberries,
+  var prevState = ""
+  var prevPlayeritem = ""
+  var myState = prevState
+  var target = (-1, -1)
+
+  val iceCreamBlueberries = Map("NONE" -> "DISH", "DISH" -> "BLUEBERRIES", "DISH-BLUEBERRIES" -> "ICE_CREAM", "DISH-BLUEBERRIES-ICE_CREAM" -> "WINDOW", "DISH-ICE_CREAM-BLUEBERRIES" -> "WINDOW")
+  val iceCreamChoppedStrawberriesBlueberries = Map("NONE" -> "STRAWBERRIES", "STRAWBERRIES" -> "CHOPPING",
+    "CHOPPED_STRAWBERRIES" -> "DISH", "DISH-CHOPPED_STRAWBERRIES" -> "ICE_CREAM", "DISH-CHOPPED_STRAWBERRIES-ICE_CREAM" -> "BLUEBERRIES",
+    "DISH-ICE_CREAM-CHOPPED_STRAWBERRIES" -> "BLUEBERRIES", "DISH-ICE_CREAM-CHOPPED_STRAWBERRIES-BLUEBERRIES" -> "WINDOW", "DISH-CHOPPED_STRAWBERRIES-ICE_CREAM-BLUEBERRIES" -> "WINDOW")
+  val сhoppedStrawberriesBlueberries = Map("NONE" -> "STRAWBERRIES", "STRAWBERRIES" -> "CHOPPING",
+    "CHOPPED_STRAWBERRIES" -> "DISH", "DISH-CHOPPED_STRAWBERRIES" -> "BLUEBERRIES", "DISH-CHOPPED_STRAWBERRIES-BLUEBERRIES" -> "WINDOW")
+  val iceCreamChoppedStrawberries = Map("NONE" -> "STRAWBERRIES", "STRAWBERRIES" -> "CHOPPING",
+    "CHOPPED_STRAWBERRIES" -> "DISH", "DISH-CHOPPED_STRAWBERRIES" -> "ICE_CREAM",
+    "DISH-ICE_CREAM-CHOPPED_STRAWBERRIES" -> "WINDOW", "DISH-CHOPPED_STRAWBERRIES-ICE_CREAM" -> "WINDOW")
+  val blueberriesCroissantChoppedStrawberries = Map("NONE" -> "DOUGH",
+    "NONE-WAITING-FOR-CROISSANT" -> /*"STRAWBERRIES"*/ "WAIT",
+    "STRAWBERRIES-WAITING-FOR-CROISSANT" -> "CHOPPING",
+    "CHOPPED_STRAWBERRIES-WAITING-FOR-CROISSANT" -> "TABLE-OVEN",
+    "CHOPPED_STRAWBERRIES-CROISSANT-IS-READY" -> "TABLE-OVEN",
+    "NONE-CROISSANT-IS-READY" -> "OVEN",
+    "DOUGH" -> "OVEN")
+  val iceCreamBlueberriesCroissantChoppedStrawberries = Map("NONE" -> "DOUGH",
+    "NONE-WAITING-FOR-CROISSANT" -> /*"STRAWBERRIES"*/ /*"WAIT*/ "DISH",
+    "DISH-WAITING-FOR-CROISSANT" -> "MOVE-OVEN",
+    "DISH-CROISSANT-IS-READY" -> "OVEN",
+    "DISH-CROISSANT" -> "TABLE-STRAWBERRIES",
+    "STRAWBERRIES-WAITING-FOR-CROISSANT" -> "CHOPPING",
+    "CHOPPED_STRAWBERRIES-WAITING-FOR-CROISSANT" -> "TABLE-OVEN",
+    "CHOPPED_STRAWBERRIES-CROISSANT-IS-READY" -> "TABLE-OVEN",
+    "NONE-CROISSANT-IS-READY" -> "OVEN",
+    "DOUGH" -> "OVEN")
+
+  val cookBook = Map("DISH-ICE_CREAM-BLUEBERRIES" -> iceCreamBlueberries,
+                     "DISH-BLUEBERRIES-ICE_CREAM" -> iceCreamBlueberries,
+                     "DISH-CHOPPED_STRAWBERRIES-ICE_CREAM-BLUEBERRIES" -> iceCreamChoppedStrawberriesBlueberries,
+                     "DISH-CHOPPED_STRAWBERRIES-BLUEBERRIES-ICE_CREAM" -> iceCreamChoppedStrawberriesBlueberries,
                      "DISH-ICE_CREAM-CHOPPED_STRAWBERRIES-BLUEBERRIES" -> iceCreamChoppedStrawberriesBlueberries,
-                     "DISH-ICE_CREAM-CHOPPED_STRAWBERRIES" -> iceCreamChoppedStrawberries)
+                     "DISH-BLUEBERRIES-CHOPPED_STRAWBERRIES-ICE_CREAM" -> iceCreamChoppedStrawberriesBlueberries,
+                     "DISH-ICE_CREAM-BLUEBERRIES-CHOPPED_STRAWBERRIES" -> iceCreamChoppedStrawberriesBlueberries,
+                     "DISH-BLUEBERRIES-ICE_CREAM-CHOPPED_STRAWBERRIES" -> iceCreamChoppedStrawberriesBlueberries,
+                     "DISH-CHOPPED_STRAWBERRIES-BLUEBERRIES" -> сhoppedStrawberriesBlueberries,
+                     "DISH-BLUEBERRIES-CHOPPED_STRAWBERRIES" -> сhoppedStrawberriesBlueberries,
+                     "DISH-ICE_CREAM-CHOPPED_STRAWBERRIES" -> iceCreamChoppedStrawberries,
+                     "DISH-CHOPPED_STRAWBERRIES-ICE_CREAM" -> iceCreamChoppedStrawberries,
+                     "DISH-BLUEBERRIES-CROISSANT-CHOPPED_STRAWBERRIES" -> blueberriesCroissantChoppedStrawberries,
+                     "DISH-ICE_CREAM-BLUEBERRIES-CHOPPED_STRAWBERRIES-CROISSANT" -> iceCreamBlueberriesCroissantChoppedStrawberries
+  )
+
 
   implicit def toNumber(point: Point): Int = point._2 * 11 + point._1 % 11
+  def toString(point: Point): String = if (point._1 < 0) "" else s"${point._1} ${point._2}"
   def toMatrix(number: Int): Point = (number % 11, number / 11)
   def walk(matrix: List[String], row: Int, col: Int) = row > 0 && row < 7 && col > 0 && col < 11 && (matrix(row)(col) == '.' || matrix(row)(col) == '0')
+  def isEmptyTable(matrix: List[String], row: Int, col: Int) = row >= 0 && row <= 7 && col >= 0 && col <= 11 && (matrix(row)(col) == '#')
   def replaceSym(matrix: List[String], oldSym: Char, newSym: Char) = matrix.map(_.replace(oldSym, newSym))
+  def searchClosestEmptyTable(matrix: List[String], point: Point) = {
+    Console.err.println(s"CLOSEST EMPTY TABLE")
+    List((point._1 - 1, point._2 - 1), (point._1, point._2 - 1),
+      (point._1 + 1, point._2 - 1), (point._1 - 1, point._2), (point._1 + 1, point._2), (point._1 - 1, point._2 + 1),
+      (point._1, point._2 + 1), (point._1 + 1, point._2 + 1)).filter(p => isEmptyTable(matrix, p._2, p._1)).head
+  }
+
+  def reach(target: String, from: Point) = {
+    Console.err.println(s"TARGETPOINT::target: $target")
+    if (target == "TABLE") ("USE", searchClosestEmptyTable(graphMatrix, from))
+    else if (target.startsWith("MOVE")) ("MOVE", targetMap(target.split('-')(1)))
+    else if (target.startsWith("TABLE")) ("USE", searchClosestEmptyTable(graphMatrix, targetMap(target.split('-')(1))))
+    else if (target == "WAIT") ("WAIT", (-1,-1)) else ("USE" ,targetMap(target))
+  }
+
   def searchSym(matrix: List[String], sym: Char) = {
     val coords = for (row <- matrix.indices; col <- matrix(row).indices; _sym = matrix(row)(col); if _sym == sym) yield (col, row)
     if (coords.nonEmpty) Some(coords(0)) else None
@@ -61,95 +82,85 @@ object Player extends App {
                    (point._1 + 1, point._2 - 1), (point._1 - 1, point._2), (point._1 + 1, point._2), (point._1 - 1, point._2 + 1),
                    (point._1, point._2 + 1), (point._1 + 1, point._2 + 1)).filter(p => walk(matrix, p._2, p._1))
 
-  def nextTarget(playerItem: String, customerItems: List[String]): List[String] = {
-    def same(component: String) = customerItems.map(_.split('-')(1)).distinct.contains(component)
-    val myCustomerItems = customerItems.map(_ + "-WINDOW")
-    Console.err.print(s"$playerItem")
-    val playerPassedStages = if (playerItem == "NONE") Array.empty[String] else playerItem.split('-')
-    val citems = myCustomerItems.map(_.split('-')(playerPassedStages.length)).distinct
-    val filteredCitems = citems.filterNot(playerPassedStages.contains)
-    Console.err.println(s" -> $filteredCitems")
-
-//    val orderMap = Map("NONE" -> List("DISH"),
-//      "DISH" -> List("BLUEBERRIES", "ICE_CREAM", "CHOPPED_STRAWBERRIES").filter(same),
-//      "DISH-ICE_CREAM" -> List("BLUEBERRIES", "CHOPPED_STRAWBERRIES"),
-//      "DISH-CHOPPED_STRAWBERRIES" -> List("BLUEBERRIES", "ICE_CREAM"),
-//      "DISH-BLUEBERRIES" -> List("ICE_CREAM", "CHOPPED_STRAWBERRIES"),
-//      "DISH-ICE_CREAM-BLUEBERRIES" -> List("WINDOW"),
-//      "DISH-BLUEBERRIES-ICE_CREAM" -> List("WINDOW"),
-//      "DISH-BLUEBERRIES-CHOPPED_STRAWBERRIES" -> List("WINDOW")
-//    )
-//    customerItems
-//    orderMap(playerItem)
-    filteredCitems
-  }
-
-  def createGraph(matrix: List[String]) = {
-    val g = new Graph
-    for (row <- matrix.indices; col <- matrix(row).indices; sym = matrix(row)(col); if walk(matrix, row, col)) {
-//      Console.err.println(s"row=$row, col=$col, sym=$sym")
-      if (walk(matrix, row - 1, col)) g.addEdge((col, row), (col, row - 1))
-      if (walk(matrix, row, col - 1)) g.addEdge((col, row), (col - 1, row))
-    }
-    g
-  }
-
-  def reach(g: Graph, matrix: List[String], player: Point, target: List[Point]) = {
-//    val (edgeTo, distTo) = g.bfs(player)
-//    val targetPoints = target.flatMap(adjTo(matrix, _))/*.filter(p => distTo(p) < 5)*/
-//    if (targetPoints.nonEmpty) "USE " + s"${targetPoints.head._1} ${targetPoints.head._2}" else "WAIT"
-    s"USE ${target.head._1} ${target.head._2}"
+  def nextTarget(myState: String, customerItems: List[Array[String]]) = {
+//    customerItems.foreach(item => Console.err.println(s"customerItem: ${item(0)} - ${item(1)}\thave: $myState"))
+    val properOrderList = customerItems.filter(orderData => cookBook(orderData(0)).contains(myState))
+    val properOrder = properOrderList.headOption.getOrElse(Array("NO SUCH ORDER! START A NEW ONE!"))(0)
+    Console.err.println(s"want to cook: $properOrder\thave: $myState")
+    val recipe = cookBook.getOrElse(properOrder, Map.empty[String, String])
+    Console.err.println(s"recipe is empty: ${recipe.isEmpty}\tmyState: $myState")
+    recipe.getOrElse(myState, "TABLE")
   }
 
   val numallcustomers = readInt
-  Console.err.println(s"numallcustomers: $numallcustomers")
+  // Console.err.println(s"numallcustomers: $numallcustomers")
 
   val customerData = (for (i <- 0 until numallcustomers) yield readLine split " ").toList
 
   val graphMatrix = (for (i <- 0 until 7) yield readLine).toList
 
   val targetMap = Map("DISH" -> searchSym(graphMatrix, 'D').getOrElse((-1,-1)),
-                      "ICE_CREAM" -> searchSym(graphMatrix, 'S').getOrElse((-1,-1)),
-                      "BLUEBERRIES" -> searchSym(graphMatrix, 'S').getOrElse((-1,-1)),
-//                      "STRAWBERRY" -> searchSym(graphMatrix, 'S').getOrElse((-1,-1)),
+                      "ICE_CREAM" -> searchSym(graphMatrix, 'I').getOrElse((-1,-1)),
+                      "BLUEBERRIES" -> searchSym(graphMatrix, 'B').getOrElse((-1,-1)),
+                      "STRAWBERRIES" -> searchSym(graphMatrix, 'S').getOrElse((-1,-1)),
                       "CHOPPING" -> searchSym(graphMatrix, 'C').getOrElse((-1,-1)),
-                      "CHOPPED_STRAWBERRIES" -> searchSym(graphMatrix, 'S').getOrElse((-1,-1)),
+                      "OVEN" -> searchSym(graphMatrix, 'O').getOrElse((-1,-1)),
+                      "DOUGH" -> searchSym(graphMatrix, 'H').getOrElse((-1,-1)),
                       "WINDOW" -> searchSym(graphMatrix, 'W').getOrElse((-1,-1)))
+//                      "EMPTY" -> searchClosestEmptyTable((graphMatrix, 'W').getOrElse((-1,-1)))
+
+  val stateBook = Map((targetMap("OVEN"), "DOUGH") -> "-WAITING-FOR-CROISSANT",
+                      (targetMap("STRAWBERRIES"), "NONE-WAITING-FOR-CROISSANT") -> "-WAITING-FOR-CROISSANT",
+                      (targetMap("DISH"), "NONE-WAITING-FOR-CROISSANT") -> "-WAITING-FOR-CROISSANT",
+                      (targetMap("CHOPPING"), "STRAWBERRIES-WAITING-FOR-CROISSANT") -> "-WAITING-FOR-CROISSANT")
+
+  def newState(prevPlayeritem: String, playeritem: String, prevTarget: Point, myState: String) = {
+//    stateBook.foreach(aa => Console.err.println(s"stateBook: $aa"))
+    Console.err.println(s"newState::prevState: $prevState\t prevTarget: $prevTarget\tstateCandidate: $myState" +
+      s"\tstateBook contains: ${stateBook.contains((prevTarget, myState))}")
+
+    playeritem + stateBook.getOrElse((prevTarget, myState), "")
+  }
 
   // game loop
   while (true) {
     val turnsremaining = readInt
-    Console.err.println(s"turnsremaining: $turnsremaining")
+    // Console.err.println(s"turnsremaining: $turnsremaining")
+
     val Array(_playerx, _playery, playeritem) = readLine split " "
+    if (playeritem != prevPlayeritem) {
+      myState = newState(prevPlayeritem, playeritem, target, myState)
+      prevPlayeritem = playeritem
+    }
+
     val playerx = _playerx.toInt
     val playery = _playery.toInt
-    Console.err.println(s"playerx: $playerx, playery: $playery, playeritem: $playeritem")
+    // Console.err.println(s"playerx: $playerx, playery: $playery, playeritem: $playeritem")
     val Array(_partnerx, _partnery, partneritem) = readLine split " "
     val partnerx = _partnerx.toInt
     val partnery = _partnery.toInt
-    Console.err.println(s"partnerx: $partnerx, partnery: $partnery, partneritem: $partneritem")
+//   Console.err.println(s"partnerx: $partnerx, partnery: $partnery, partneritem: $partneritem")
     val numtableswithitems = readInt // the number of tables in the kitchen that currently hold an item
-    Console.err.println(s"numtableswithitems: $numtableswithitems")
+     Console.err.println(s"numtableswithitems: $numtableswithitems")
     for (i <- 0 until numtableswithitems) {
       val Array(_tablex, _tabley, item) = readLine split " "
       val tablex = _tablex.toInt
       val tabley = _tabley.toInt
-      Console.err.println(s"\ttablex: $tablex, tabley: $tabley")
+       Console.err.println(s"\t($tablex $tabley) - $item")
     }
     // ovencontents: ignore until wood 1 league
     val Array(ovencontents, _oventimer) = readLine split " "
+    if (ovencontents == "CROISSANT") myState = myState.replace("WAITING-FOR-CROISSANT", "CROISSANT-IS-READY")
     val oventimer = _oventimer.toInt
-    Console.err.println(s"ovencontents: $ovencontents, oventimer: $oventimer")
+   Console.err.println(s"ovencontents: $ovencontents, oventimer: $oventimer")
     val numcustomers = readInt // the number of customers currently waiting for food
-    Console.err.println(s"numcustomers: $numcustomers")
-    val customers = (for (i <- 0 until numcustomers) yield readLine split " ").toList.sortBy(_(0))
-    customers.foreach(c => Console.err.println(s"customeritem: ${c(0)}, customeraward: ${c(1)}"))
+    // Console.err.println(s"numcustomers: $numcustomers")
+    val customers = (for (i <- 0 until numcustomers) yield readLine split " ").toList.sortBy(_(1))
+//    customers.foreach(c => Console.err.println(s"customeritem: ${c(0)}, customeraward: ${c(1)}"))
+    val trg = nextTarget(myState, customers/*.map(_(0))*/)
+    val action = reach(trg, (playerx, playery))
+    if (action._2._1 > 0) target = action._2
 
-    val g = createGraph(graphMatrix)
-    val target = nextTarget(playeritem, customers.map(_(0)))
-    Console.err.println(s"target is empty: ${target.isEmpty}")
-    target.foreach(t => Console.err.println(s"TARGET: $t"))
-    val action = reach(g, graphMatrix, (playerx, playery), target.map(targetMap.apply))
-    println(action)
+    println(s"${action._1} ${toString(action._2)}")
   }
 }
--- src/test/scala/codingame/challenge/codealamode/PlayerTests.scala
+++ src/test/scala/codingame/challenge/codealamode/PlayerTests.scala
@@ -18,12 +18,6 @@ class PlayerTests extends FlatSpec {
                       "ICE_CREAM" -> searchSym(testMatrix, 'W').getOrElse((-1,-1)),
                       "BLUEBERRIES" -> searchSym(testMatrix, 'B').getOrElse((-1,-1)),
                       "WINDOW" -> searchSym(testMatrix, 'I').getOrElse((-1,-1)))
-  
-  "A Player" should "breadth-first search" in {
-    val g = Player.createGraph(testMatrix)
-    val bfs = g.bfs(Player.toNumber((1, 1)))
-    Console.err.println(s"$bfs")
-  }
 
   "A Player" should "convert number to matrix and back" in {
     val point = (10, 2)
@@ -44,38 +38,8 @@ class PlayerTests extends FlatSpec {
     assert(res2 == List((9, 2), (9, 4)))
     assert(res3 == List((9, 5)))
   }
-  "A Player" should "find another target" in {
-    val customeritems = List("DISH-ICE_CREAM-BLUEBERRIES", "DISH-ICE_CREAM-BLUEBERRIES", "DISH-ICE_CREAM-BLUEBERRIES")
-    val playeritem = "DISH-ICE_CREAM"
-    val nextTarget = Player.nextTarget(playeritem, customeritems)
-    Console.err.println(s"nextTarget: $nextTarget")
-    assert(nextTarget == List("BLUEBERRIES"))
-  }
 
-  "A Player" should "find next target" in {
-    val customeritems = List("DISH-CHOPPED_STRAWBERRIES-BLUEBERRIES", "DISH-ICE_CREAM-BLUEBERRIES", "DISH-ICE_CREAM-CHOPPED_STRAWBERRIES")
-//    val customeritemsA = List("DISH-BLUEBERRIES-ICE_CREAM", "DISH-ICE_CREAM-BLUEBERRIES", "DISH-ICE_CREAM-BLUEBERRIES")
-    Console.err.println(s"customer items: $customeritems")
-    val result = List(List("DISH"), List("BLUEBERRIES"), List("ICE_CREAM"), List("WINDOW"), List("BLUEBERRIES", "ICE_CREAM"))
-    val playeritem = List("NONE", "DISH-ICE_CREAM", "DISH-BLUEBERRIES", "DISH-BLUEBERRIES-ICE_CREAM", "DISH")
-
-    val nextTarget = playeritem.map(Player.nextTarget(_, customeritems)).zipWithIndex
-    Console.err.println
-    nextTarget.foreach {
-      next => {
-        Console.err.println(s"prev state: ${playeritem(next._2)}, target: ${next._1}")
-//        assert(next._1 == result(next._2))
-      }
-    }
-  }
-  
-  "A Player" should "reach target" in {
-    val playeritem = "NONE"
-    val customeritems = List("DISH-BLUEBERRIES-ICE_CREAM", "DISH-ICE_CREAM-BLUEBERRIES", "DISH-ICE_CREAM-BLUEBERRIES")
-    val nextTarget = Player.nextTarget(playeritem, customeritems)
-    val g = Player.createGraph(testMatrix)
-    val player = Player.searchSym(testMatrix, '0').getOrElse((-1,-1))
-    val res = Player.reach(g, testMatrix, player, nextTarget.map(targetMap.apply))
-    Console.err.println(s"res: $res")
+  "A Player" should "print target map" in {
+    Console.err.println(s"${Player.stateBook}")
   }
 }
