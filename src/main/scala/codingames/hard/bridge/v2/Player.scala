aa2c58c3005eb771f064a5d710e8774a1a446b98
JolyRoger <mjelnr@gmail.com>
2019-03-24 01:04:20 +0100

    12 test works

--- src/main/scala/codingames/hard/bridge/v2/Player.scala
+++ src/main/scala/codingames/hard/bridge/v2/Player.scala
@@ -1,10 +1,10 @@
 package codingames.hard.bridge.v2
 
 object Player extends App {
-  type Point = (Int, Int)
-  type PathData = (Int, (Int, Int, Int))
+  type Point = (Int, Int, Int)
+  type PathData = (Int, Point)
   type MotoData = (Int, Int, Int, List[(Int, Int)])
-  type Rule = (Int, Int, Int) => MotoData
+  type Rule = Point => MotoData
   type Command = Map[String, Rule]
 
   val m = readInt // the amount of motorbikes to control
@@ -14,23 +14,31 @@ object Player extends App {
   val l2 = readLine
   val l3 = readLine
 
-  def commands = Map("SPEED" -> ((x: Int, y: Int, s: Int) => {
+  /*val*/ def order = Map(0 -> "SPEED", 1 -> "JUMP", 2 -> "WAIT", 3 -> "UP", 4 -> "DOWN", 5 -> "SLOW")
+
+  /*val*/ def commands = Map(0 -> ((x: Int, y: Int, s: Int) => {
     (x + s + 1, y, s + 1, (x to x + s + 1).map((_, y)).toList)
-  }), "SLOW" -> ((x: Int, y: Int, s: Int) => {
+  }), 5 -> ((x: Int, y: Int, s: Int) => {
     (x + s - 1, y, s - 1, (x until x + s).map((_, y)).toList)
-  }), "JUMP" -> ((x: Int, y: Int, s: Int) => {
+  }), 1 -> ((x: Int, y: Int, s: Int) => {
     (x + s, y, s, List((x, y), (x + s, y)))
-  }), "WAIT" -> ((x: Int, y: Int, s: Int) => {
+  }), 2 -> ((x: Int, y: Int, s: Int) => {
     (x + s, y, s, (x to x + s).map((_, y)).toList)
-  }), "UP" -> ((x: Int, y: Int, s: Int) => {
-    (x + s, y - 1, s, ((x until x + s).map((_, y)) :: (x + 1 to x + s).map((_, y - 1)) :: Nil).flatten)
-  }), "DOWN" -> ((x: Int, y: Int, s: Int) => {
-    (x + s, y + 1, s, ((x until x + s).map((_, y)) :: (x + 1 to x + s).map((_, y + 1)) :: Nil).flatten)
+  }), 3 -> ((x: Int, y: Int, s: Int) => {
+    (x + s, y - 1, s, if (s == 0) List((x, y), (x, y - 1)) else ((x until x + s).map((_, y)) :: (x + 1 to x + s).map((_, y - 1)) :: Nil).flatten)
+  }), 4 -> ((x: Int, y: Int, s: Int) => {
+    (x + s, y + 1, s, if (s == 0) List((x, y), (x, y + 1)) else ((x until x + s).map((_, y)) :: (x + 1 to x + s).map((_, y + 1)) :: Nil).flatten)
   }))
+
+  var usedSquares = Set.empty[Point]
 
   val lines = Array(l0, l1, l2, l3)
 
-  val roadLength = lines(0).length
+  Console.err.println(s"LINES")
+  printLines(lines.map(_.toCharArray))
+  Console.err.println
+
+  /*val*/ def roadLength = lines(0).length
 
   var motolines = lines.map(_.toCharArray)
   Console.err.println(s"the amount of motorbikes to control: $m")
@@ -40,24 +48,19 @@ object Player extends App {
 
   var step = 0
 
-  val order = Map(0 -> "SPEED", 1 -> "JUMP", 2 -> "WAIT", 3 -> "UP", 4 -> "DOWN", 5 -> "SLOW")
-
-
-  def put(stack: List[PathData], e: PathData) = {
-    e :: stack
-  }
 
   def newXYS(stack: List[PathData]) = {
     val (c, (x, y, s)) = stack.head
-    commands(order(c))(x, y, s)
+    commands(c)(x, y, s)
   }
 
   def isActive(stack: List[PathData]) = stack.size < 50 && stack.head._2._1 < roadLength
 
-  def can(data: MotoData) = data._4.forall(xy => {
+  def can(data: MotoData, oldXYS: (Int, Int, Int)) = data._4.forall(xy => {
       val (sym, line) = xy
       line >= 0 && line < 4 &&
-        sym >= 0 && (if (sym < roadLength) lines(line)(sym) == '.' else true)
+      sym >= 0 && (if (sym < roadLength) lines(line)(sym) == '.' else true) &&
+      (data._1 != oldXYS._1 || data._2 != oldXYS._2 || data._3 != oldXYS._3) && !usedSquares.contains(data._1, data._2, data._3)
     })
 
   def update(stack: List[PathData]) = {
@@ -74,28 +77,32 @@ object Player extends App {
   }
 
   def calc(initX: Int, initY: Int, initS: Int) = {
+    Console.err.println(s"init: $initX $initY $initS")
     var stack = (0, (initX, initY, initS)) :: Nil
     do {
       val (newX, newY, newS, points) = newXYS(stack)
-      stack = if (can((newX, newY, newS, points))) put(stack, (0, (newX, newY, newS))) else update(stack)
+      stack = if (can((newX, newY, newS, points), stack.head._2)) {
+        usedSquares += ((newX, newY, newS))
+        (0, (newX, newY, newS)) :: stack
+      } else update(stack)
     } while (isActive(stack))
-    stack.tail
+    stack
   }.reverse
 
   var instructions: List[PathData] = List.empty
 
   while (true) {
-    val s = readInt // the motorbikes' speed
+    val s = readInt
     for (i <- 0 until m) {
       val Array(x, y, a) = for (j <- readLine split " ") yield j.toInt
       if (step == 0) instructions = calc(x, y, s)
       if (x < motolines(y).length) motolines(y)(x) = '*'
+//      Console.err.println(s"x[$i]=$x y[$i]=$y s[$i]=$s")
     }
-    printLines(motolines)
 
-    val action = order(instructions(step)._1)
+//    printLines(motolines)
+    println(order(instructions(step)._1))
     step = step + 1
-    println(action)
     motolines = lines.map(_.toCharArray)
   }
 }
--- src/test/scala/codingame/hard/bridge/v2/PlayerTest.scala
+++ src/test/scala/codingame/hard/bridge/v2/PlayerTest.scala
@@ -5,8 +5,8 @@ import org.scalatest.FlatSpec
 
 class PlayerTests extends FlatSpec {
   "A Player" should "calc" in {
-    val order = Map(0 -> "SPEED", 1 -> "JUMP", 2 -> "WAIT", 3 -> "UP", 4 -> "DOWN", 5 -> "SLOW", 6-> "EMPTY")
-    val stack = Player.calc(0, 2, 0)
+    val order = Map(0 -> "SPEED", 1 -> "JUMP", 2 -> "WAIT", 3 -> "UP", 4 -> "DOWN", 5 -> "SLOW")
+    val stack = Player.calc(0, 2, 1)
     Console.err.println
     stack.foreach(command => Console.err.println(s"${order(command._1)} "))
   }
