d4b92022dc04fab096522a7899fbfaf7e6680b73
JolyRoger <mjelnr@gmail.com>
2019-03-27 21:11:07 +0100

    refactroring

--- src/main/scala/codingames/hard/bridge/v2/Player.scala
+++ src/main/scala/codingames/hard/bridge/v2/Player.scala
@@ -1,11 +1,10 @@
-package codingames.hard.bridge.v2
+//package codingames.hard.bridge.v2
 
 object Player extends App {
-  type Point = (Int, Int, Int)
-  type PathData = (Int, Point)
-  type MotoData = (Int, Int, Int, List[(Int, Int)])
-  type Rule = Point => MotoData
-  type Command = Map[String, Rule]
+  type Point = Array[Int]
+  type StackItem = (Int, List[Point])
+  type Stack = List[StackItem]
+  type MotoData = (Point, List[Point])
 
   val m = readInt // the amount of motorbikes to control
   val v = readInt // the minimum amount of motorbikes that must survive
@@ -14,61 +13,54 @@ object Player extends App {
   val l2 = readLine
   val l3 = readLine
 
-  /*val*/ def order = Map(0 -> "SPEED", 1 -> "JUMP", 2 -> "WAIT", 3 -> "UP", 4 -> "DOWN", 5 -> "SLOW")
+  val order = Map(0 -> "SPEED", 1 -> "JUMP", 2 -> "WAIT", 3 -> "UP", 4 -> "DOWN", 5 -> "SLOW")
 
-  /*val*/ def commands = Map(0 -> ((x: Int, y: Int, s: Int) => {
-    (x + s + 1, y, s + 1, (x to x + s + 1).map((_, y)).toList)
-  }), 5 -> ((x: Int, y: Int, s: Int) => {
-    (x + s - 1, y, s - 1, (x until x + s).map((_, y)).toList)
-  }), 1 -> ((x: Int, y: Int, s: Int) => {
-    (x + s, y, s, List((x, y), (x + s, y)))
-  }), 2 -> ((x: Int, y: Int, s: Int) => {
-    (x + s, y, s, (x to x + s).map((_, y)).toList)
-  }), 3 -> ((x: Int, y: Int, s: Int) => {
-    (x + s, y - 1, s, if (s == 0) List((x, y), (x, y - 1)) else ((x until x + s).map((_, y)) :: (x + 1 to x + s).map((_, y - 1)) :: Nil).flatten)
-  }), 4 -> ((x: Int, y: Int, s: Int) => {
-    (x + s, y + 1, s, if (s == 0) List((x, y), (x, y + 1)) else ((x until x + s).map((_, y)) :: (x + 1 to x + s).map((_, y + 1)) :: Nil).flatten)
+  val commands = Map(0 -> ((x: Int, y: Int, a: Int, s: Int) => {
+    (Array(x + s + 1, y, a, s + 1), (x to x + s + 1).map(Array(_, y)).toList)
+  }), 5 -> ((x: Int, y: Int, a: Int, s: Int) => {
+    (Array(x + s - 1, y, a, s - 1), (x until x + s).map(Array(_, y)).toList)
+  }), 1 -> ((x: Int, y: Int, a: Int, s: Int) => {
+    (Array(x + s, y, a, s), List(Array(x, y), Array(x + s, y)))
+  }), 2 -> ((x: Int, y: Int, a: Int, s: Int) => {
+    (Array(x + s, y, a, s), (x to x + s).map(Array(_, y)).toList)
+  }), 3 -> ((x: Int, y: Int, a: Int, s: Int) => {
+    (Array(x + s, y - 1, a, s), if (s == 0) List(Array(x, y), Array(x, y - 1)) else ((x until x + s).map(Array(_, y)) :: (x + 1 to x + s).map(Array(_, y - 1)) :: Nil).flatten)
+  }), 4 -> ((x: Int, y: Int, a: Int, s: Int) => {
+    (Array(x + s, y + 1, a, s), if (s == 0) List(Array(x, y), Array(x, y + 1)) else ((x until x + s).map(Array(_, y)) :: (x + 1 to x + s).map(Array(_, y + 1)) :: Nil).flatten)
   }))
 
-  var usedSquares = Set.empty[Point]
-
   val lines = Array(l0, l1, l2, l3)
-
-  Console.err.println(s"LINES")
-  printLines(lines.map(_.toCharArray))
-  Console.err.println
-
-  /*val*/ def roadLength = lines(0).length
-
-  var motolines = lines.map(_.toCharArray)
-  Console.err.println(s"the amount of motorbikes to control: $m")
-  Console.err.println(s"the minimum amount of motorbikes that must survive: $v")
-
-  def printLines(lines: Array[Array[Char]]) = lines.foreach(line => Console.err.println(s"lanes of the road: ${line.mkString("", "", "")}"))
-
+  val roadLength = lines(0).length
   var step = 0
 
+  def string(lst: List[Array[Int]]) = lst.map(_.mkString("", "", "")).reduce(_ + ":" + _)
 
-  def newXYS(stack: List[PathData]) = {
-    val (c, (x, y, s)) = stack.head
-    commands(c)(x, y, s)
+  def newXYS(next: StackItem) = {
+    val (c, pathData) = next
+    pathData.map(pd => survive(commands(c)(pd(0), pd(1), pd(2), pd(3))))
   }
 
-  def isActive(stack: List[PathData]) = stack.size < 50 && stack.head._2._1 < roadLength
+  def isActive(stack: Stack) = stack.isEmpty || (stack.size < 50 && stack.head._2.head(0) < roadLength)
 
-  def can(data: MotoData, oldXYS: (Int, Int, Int)) = data._4.forall(xy => {
-      val (sym, line) = xy
-      line >= 0 && line < 4 &&
-      sym >= 0 && (if (sym < roadLength) lines(line)(sym) == '.' else true) &&
-      (data._1 != oldXYS._1 || data._2 != oldXYS._2 || data._3 != oldXYS._3) && !usedSquares.contains(data._1, data._2, data._3)
-    })
+  def notContain(pointSet: Set[String], item: List[Point]) = !pointSet.contains(string(item))
 
-  def update(stack: List[PathData]) = {
+  def can(liveMoto: List[MotoData], oldXYS: List[Point], usedSquares: Set[String], mustSurvive: Int) = {
+    liveMoto.size >= mustSurvive &&
+          liveMoto.zipWithIndex.forall(motoWithIndex => {
+            val (moto, index) = motoWithIndex
+            moto._1(1) >= 0 && moto._1(1) < lines.length &&
+            (moto._1(0) != oldXYS(index)(0) || moto._1(1) != oldXYS(index)(1) || moto._1(3) != oldXYS(index)(3)) &&
+              !usedSquares.contains(string(liveMoto.map(_._1)))
+          })
+  }
+
+  def update(stack: Stack): Stack = {
     var stackHead = stack.head
     var outStack = stack.tail
     var command = stackHead._1 + 1
 
-    while (command > 5) {
+    while (command > order.size) {
+      if (outStack.isEmpty) return outStack // can't find a way
       stackHead = outStack.head
       outStack = outStack.tail
       command = stackHead._1 + 1
@@ -76,33 +68,39 @@ object Player extends App {
     (command, stackHead._2) :: outStack
   }
 
-  def calc(initX: Int, initY: Int, initS: Int) = {
-    Console.err.println(s"init: $initX $initY $initS")
-    var stack = (0, (initX, initY, initS)) :: Nil
+  def survive(moto: MotoData) = {
+    (if (moto._2.forall(point => {
+      point(0) < 0 || point(0) >= roadLength || point(1) < 0 || point(1) >= lines.length || lines(point(1))(point(0)) == '.'
+    })) moto._1 else Array(moto._1(0), moto._1(1), 0, moto._1(3)), moto._2)
+  }
+
+  def successful(stackItem: StackItem) = stackItem._2.exists(_(0) >= roadLength)
+
+  def calc(init: List[Point], mustSurvive: Int): Stack = {
+    var stack = (0, init) :: Nil
+    var usedSquares = Set.empty[String]
+
     do {
-      val (newX, newY, newS, points) = newXYS(stack)
-      stack = if (can((newX, newY, newS, points), stack.head._2)) {
-        usedSquares += ((newX, newY, newS))
-        (0, (newX, newY, newS)) :: stack
+      if (stack.isEmpty) return stack
+      val newCoords = newXYS(stack.head).filter(_._1(2) == 1)
+      stack = if (can(newCoords, stack.head._2, usedSquares, mustSurvive)) {
+        val ncm = newCoords.map(_._1)
+        usedSquares += string(ncm)
+        (0, ncm) :: stack
       } else update(stack)
     } while (isActive(stack))
     stack
   }.reverse
 
-  var instructions: List[PathData] = List.empty
+  var instructions: Stack = List.empty
 
   while (true) {
     val s = readInt
-    for (i <- 0 until m) {
-      val Array(x, y, a) = for (j <- readLine split " ") yield j.toInt
-      if (step == 0) instructions = calc(x, y, s)
-      if (x < motolines(y).length) motolines(y)(x) = '*'
-//      Console.err.println(s"x[$i]=$x y[$i]=$y s[$i]=$s")
-    }
+    val moto = (for (i <- 0 until m) yield ((readLine + s" $s") split " ").map(_.toInt)).toList
+    val stack = (v to m).map(calc(moto, _)).reverse.find(item => item.nonEmpty && successful(item.last))
+    if (step == 0) instructions = stack.get
 
-//    printLines(motolines)
     println(order(instructions(step)._1))
     step = step + 1
-    motolines = lines.map(_.toCharArray)
   }
 }
--- src/test/scala/codingame/hard/bridge/v2/PlayerTest.scala
+++ src/test/scala/codingame/hard/bridge/v2/PlayerTest.scala
@@ -1,13 +1,88 @@
 package codingame.hard.bridge.v2
 
 import codingames.hard.bridge.v2.Player
+import codingames.hard.bridge.v2.Player.{MotoData, Point}
 import org.scalatest.FlatSpec
 
 class PlayerTests extends FlatSpec {
+
+
+  /*val*/ def linesOrig = Array("................000000000........00000........000.............00.",
+                                "...0..................000....000......0.0..................00000.",
+                                "....000.........0.0...000................000............000000.0.",
+                                "............0.000000...........0000...............0.0.....000000.")
+//  "A Player" should "calc" in {
+//    val order = Map(0 -> "SPEED", 1 -> "JUMP", 2 -> "WAIT", 3 -> "UP", 4 -> "DOWN", 5 -> "SLOW")
+//    val stack = Player.calc(0, 2, 1)
+//    Console.err.println
+//    stack.foreach(command => Console.err.println(s"${order(command._1)} "))
+//  }
+
   "A Player" should "calc" in {
-    val order = Map(0 -> "SPEED", 1 -> "JUMP", 2 -> "WAIT", 3 -> "UP", 4 -> "DOWN", 5 -> "SLOW")
-    val stack = Player.calc(0, 2, 1)
-    Console.err.println
-    stack.foreach(command => Console.err.println(s"${order(command._1)} "))
+    val init = List(Array(0, 0, 1, 3), Array(0, 1, 1, 3), Array(0, 2, 1, 3), Array(0, 3, 1, 3))
+    val stack = Player.calc(init, 1)
+    stack.foreach(command => Console.err.println(s"${Player.order(command._1)} "))
+  }
+
+  "A Player" should "find new x y s" in {
+    val init = List(Array(0, 0, 1, 6), Array(0, 1, 1, 6), Array(0, 2, 1, 6), Array(0, 3, 1, 6))
+    val res = Player.newXYS((0, init))
+    res.foreach(r => Console.err.println(s"(${r._1.mkString("Array(", ", ", ")")}, ${r._2.map(_.mkString("Array(", ", ", ")"))})"))
+  }
+  "A Player" should "check if is active" in {
+    val first = (0, List(Array(0, 0, 1, 6), Array(0, 1, 1, 6), Array(0, 2, 1, 6), Array(0, 3, 1, 6)))
+    val second = (0, List(Array(7, 0, 1, 7), Array(7, 1, 1, 7), Array(7, 2, 1, 7), Array(7, 3, 1, 7)))
+    val stack = List(first, second)
+    val res = Player.isActive(stack)
+    Console.err.println(s"res: $res")
+  }
+  "A Player" should "check if point is valid" in {
+    val oldXYS = List( Array(0, 0, 1, 6), Array(0, 1, 1, 6), Array(0, 2, 1, 6), Array(0, 3, 1, 6))
+    val newXYS = List((Array(7, 0, 1, 7), List(Array(0, 0), Array(1, 0), Array(2, 0), Array(3, 0), Array(4, 0), Array(5, 0), Array(6, 0), Array(7, 0))),
+                      (Array(7, 1, 1, 7), List(Array(0, 1), Array(1, 1), Array(2, 1), Array(3, 1), Array(4, 1), Array(5, 1), Array(6, 1), Array(7, 1))),
+                      (Array(7, 2, 1, 7), List(Array(0, 2), Array(1, 2), Array(2, 2), Array(3, 2), Array(4, 2), Array(5, 2), Array(6, 2), Array(7, 2))),
+                      (Array(7, 3, 1, 7), List(Array(0, 3), Array(1, 3), Array(2, 3), Array(3, 3), Array(4, 3), Array(5, 3), Array(6, 3), Array(7, 3))))
+
+    val yeswecan = Player.can(newXYS, oldXYS, Set.empty[String], 3)
+    Console.err.println(s"yeswecan=$yeswecan")
+  }
+
+  "A Player" should "test for contain" in {
+    val item = List((Array(7, 0, 1, 7), List(Array(0, 0), Array(1, 0), Array(2, 0), Array(3, 0), Array(4, 0), Array(5, 0), Array(6, 0), Array(7, 0))),
+                    (Array(7, 1, 1, 7), List(Array(0, 1), Array(1, 1), Array(2, 1), Array(3, 1), Array(4, 1), Array(5, 1), Array(6, 1), Array(7, 1))),
+                    (Array(7, 2, 1, 7), List(Array(0, 2), Array(1, 2), Array(2, 2), Array(3, 2), Array(4, 2), Array(5, 2), Array(6, 2), Array(7, 2))),
+                    (Array(7, 3, 1, 7), List(Array(0, 3), Array(1, 3), Array(2, 3), Array(3, 3), Array(4, 3), Array(5, 3), Array(6, 3), Array(7, 3)))).map(_._1)
+
+    val theSameItem = List((Array(7, 0, 1, 7), List(Array(0, 0), Array(1, 0), Array(2, 0), Array(3, 0), Array(4, 0), Array(5, 0), Array(6, 0), Array(7, 0))),
+      (Array(7, 1, 1, 7), List(Array(0, 1), Array(1, 1), Array(2, 1), Array(3, 1), Array(4, 1), Array(5, 1), Array(6, 1), Array(7, 1))),
+      (Array(7, 2, 1, 7), List(Array(0, 2), Array(1, 2), Array(2, 2), Array(3, 2), Array(4, 2), Array(5, 2), Array(6, 2), Array(7, 2))),
+      (Array(7, 3, 1, 7), List(Array(0, 3), Array(1, 3), Array(2, 3), Array(3, 3), Array(4, 3), Array(5, 3), Array(6, 3), Array(7, 3)))).map(_._1)
+
+    val pointSet = Set(Player.string(item))
+
+    val res = Player.notContain(pointSet, theSameItem)
+
+    Console.err.println(s"res: $res")
+  }
+
+  "A Player" should "check for succesfull" in {
+    val item = (0, List(Array(50, 0, 1, 3), Array(50, 1, 1, 3), Array(76, 2, 1, 3), Array(2, 3, 0, 3)))
+    val res = Player.successful(item)
+    Console.err.println(s"res: $res")
+  }
+
+  "A Player" should "transform new coords" in {
+    val newXYS = List((Array(7, 0, 1, 7), List(Array(0, 0), Array(1, 0), Array(2, 0), Array(3, 0), Array(4, 0), Array(5, 0), Array(6, 0), Array(7, 0))),
+                      (Array(7, 1, 1, 7), List(Array(0, 1), Array(1, 1), Array(2, 1), Array(3, 1), Array(4, 1), Array(5, 1), Array(6, 1), Array(7, 1))),
+                      (Array(7, 2, 1, 7), List(Array(0, 2), Array(1, 2), Array(2, 2), Array(3, 2), Array(4, 2), Array(5, 2), Array(6, 2), Array(7, 2))),
+                      (Array(7, 3, 1, 7), List(Array(0, 3), Array(1, 3), Array(2, 3), Array(3, 3), Array(4, 3), Array(5, 3), Array(6, 3), Array(7, 3))))
+    Console.err.println(s"before")
+    Console.err.println(s"(${newXYS.head._1.mkString("Array(", ", ", ")")}, ${newXYS.head._2.map(_.mkString("Array(", ", ", ")"))})")
+    Console.err.println(s"(${newXYS.last._1.mkString("Array(", ", ", ")")}, ${newXYS.last._2.map(_.mkString("Array(", ", ", ")"))})")
+    val transformedCoordsHead = Player.survive(newXYS.head)
+    val transformedCoordsLast = Player.survive(newXYS.last)
+    Console.err.println(s"after")
+    Console.err.println(s"(${transformedCoordsHead._1.mkString("Array(", ", ", ")")}, ${transformedCoordsHead._2.map(_.mkString("Array(", ", ", ")"))})")
+    Console.err.println(s"(${transformedCoordsLast._1.mkString("Array(", ", ", ")")}, ${transformedCoordsLast._2.map(_.mkString("Array(", ", ", ")"))})")
   }
 }
