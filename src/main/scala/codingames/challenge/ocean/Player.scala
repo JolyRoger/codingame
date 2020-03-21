92f2326c351f1b9f5ae75977a595460ad3788a41
JolyRoger <mjelnr@gmail.com>
2020-03-21 14:54:16 +0100

    log

--- src/main/scala/codingames/challenge/ocean/Player.scala
+++ src/main/scala/codingames/challenge/ocean/Player.scala
@@ -1,4 +1,4 @@
-package codingames.challenge.ocean
+//package codingames.challenge.ocean
 
 import math._
 import scala.util._
@@ -9,25 +9,29 @@ import scala.io.StdIn._
  * the standard input according to the problem statement.
  **/
 object Player extends App {
-    val Array(width, height, myId) = (readLine split " ").map (_.toInt)
-    for(i <- 0 until height) {
-        val line = readLine
-    }
-    
+  val Array(width, height, myId) = (readLine split " ").map(_.toInt)
+  Console.err.println(s"myId=$myId size=$width:$height")
+  for (i <- 0 until height) {
+    val line = readLine
+    Console.err.println(s"line=$line")
+  }
+
+  // Write an action using println
+  // To debug: Console.err.println("Debug messages...")
+
+  println("0 13")
+
+  // game loop
+  while (true) {
+    val Array(x, y, myLife, oppLife, torpedoCooldown, sonarCooldown, silenceCooldown, mineCooldown) = (readLine split " ").map(_.toInt)
+    val sonarResult = readLine
+    Console.err.println(s"sonarResult=$sonarResult")
+    val opponentOrders = readLine
+    Console.err.println(s"opponentOrders=$opponentOrders")
+
     // Write an action using println
     // To debug: Console.err.println("Debug messages...")
-    
-    println("7 7")
 
-    // game loop
-    while(true) {
-        val Array(x, y, myLife, oppLife, torpedoCooldown, sonarCooldown, silenceCooldown, mineCooldown) = (readLine split " ").map (_.toInt)
-        val sonarResult = readLine
-        val opponentOrders = readLine
-        
-        // Write an action using println
-        // To debug: Console.err.println("Debug messages...")
-        
-        println("MOVE N TORPEDO")
-    }
+    println("MOVE N TORPEDO")
+  }
 }
