c7f569f14d34db1764c3df5644e0a6e49f45510d
JolyRoger <mjelnr@gmail.com>
2020-03-22 20:25:19 +0100

    direction select

--- src/main/scala/codingames/challenge/ocean/Player.scala
+++ src/main/scala/codingames/challenge/ocean/Player.scala
@@ -10,23 +10,39 @@ class Square(x: Int, y: Int, sym: Char) {
   val getX = x
   val getY = y
   val getSym = sym
-  def print = Console.err.print(s"$x:$y:$sym ")
+  val rock = sym == 'x'
+  var accessible = sym == '.'
+  def print = Console.err.print(s"$sym")
+//  def print = Console.err.print(s"$x:$y:$sym ")
 }
 
 class SquareManager(var myPosition: Square, board: Array[Array[Square]]) {
-  def possibleDirection = ee
+
+  myPosition.accessible = false
+
+  def setMyPosition(square: Square) = {
+    myPosition = square
+    myPosition.accessible = false
+  }
+
+
+  def possibleDirection = {
+    val candidates = Array((myPosition.getX + 1, myPosition.getY, 'E'), (myPosition.getX - 1, myPosition.getY, 'W'), (myPosition.getX, myPosition.getY + 1, 'S'), (myPosition.getX, myPosition.getY - 1, 'N'))
+    candidates.filter(xy => xy._1 >= 0 && xy._1 < 15 &&
+                            xy._2 >= 0 && xy._2 < 15 &&
+                            board(xy._2)(xy._1).accessible)
+      .map(_._3)
+  }
 }
 
 object Player extends App {
 //------------------------------------------FILE ENTRY------------------------------------------------------------------
-/*
-  val filename = "ocean/ocean0.txt"
-  val bufferedSource = Source.fromFile(filename)
-  val data = bufferedSource.getLines
-
-  def readInt = if (data.hasNext) data.next.toInt else -1
-  def readLine = if (data.hasNext) data.next else "EOF"
-*/
+//  val filename = "ocean/ocean0.txt"
+//  val bufferedSource = Source.fromFile(filename)
+//  val data = bufferedSource.getLines
+//
+//  def readInt = if (data.hasNext) data.next.toInt else -1
+//  def readLine = if (data.hasNext) data.next else "EOF"
 //----------------------------------------------------------------------------------------------------------------------
 
   val Array(width, height, myId) = (readLine split " ").map(_.toInt)
@@ -35,7 +51,7 @@ object Player extends App {
   val boardSym = (for (i <- 0 until height) yield readLine).map(_.toCharArray)
   val board = boardSym.zipWithIndex.map(arrIndex => arrIndex._1.zipWithIndex.map(symIndex => new Square(symIndex._2, arrIndex._2, symIndex._1))).toArray
 
-  val legalSquares = board.flatten.filter(_.getSym == '.')
+  val legalSquares = board.flatten.filter(_.accessible)
   val rand = new Random(System.currentTimeMillis)
   val randSquare = legalSquares(rand.nextInt(legalSquares.length))
 
@@ -44,7 +60,7 @@ object Player extends App {
 
 //  board.foreach(bl => {
 //    bl.foreach(_.print)
-//    Console.err.println(s"\n")})
+//    Console.err.println})
 
   // Write an action using println
   // To debug: Console.err.println("Debug messages...")
@@ -55,6 +71,8 @@ object Player extends App {
   // game loop
   while (true) {
     val Array(x, y, myLife, oppLife, torpedoCooldown, sonarCooldown, silenceCooldown, mineCooldown) = (readLine split " ").map(_.toInt)
+    manager.setMyPosition(board(y)(x))
+    Console.err.println(s"$x $y $myLife $oppLife $torpedoCooldown $sonarCooldown $silenceCooldown $mineCooldown")
     val sonarResult = readLine
     Console.err.println(s"sonarResult=$sonarResult")
     val opponentOrders = readLine
@@ -62,7 +80,9 @@ object Player extends App {
 
     // Write an action using println
     // To debug: Console.err.println("Debug messages...")
+    val directions = manager.possibleDirection
+    val index = rand.nextInt(directions.length)
 
-    println("MOVE N TORPEDO")
+    println(s"MOVE ${directions(index)} TORPEDO")
   }
 }
--- src/test/scala/codingame/challenge/ocean/PlayerTest.scala
+++ src/test/scala/codingame/challenge/ocean/PlayerTest.scala
@@ -1,12 +1,52 @@
 package codingame.challenge.ocean
 
-import codingames.challenge.ocean.Player
-import org.scalatest.FlatSpec
+import codingames.challenge.ocean.Player.readLine
+import codingames.challenge.ocean.{Player, Square, SquareManager}
+import org.scalatest.{BeforeAndAfter, FlatSpec}
 
-class PlayerTest extends FlatSpec {
+import scala.io.Source
+import scala.util.Random
 
+class PlayerTest extends FlatSpec with BeforeAndAfter {
+
+//------------------------------------------FILE ENTRY------------------------------------------------------------------
+  val filename = "ocean/ocean0.txt"
+  val bufferedSource = Source.fromFile(filename)
+  val data = bufferedSource.getLines
+
+  def readInt = if (data.hasNext) data.next.toInt else -1
+  def readLine = if (data.hasNext) data.next else "EOF"
+//------------------------------------------FILE ENTRY------------------------------------------------------------------
+
+  val Array(width, height, myId) = (readLine split " ").map(_.toInt)
+  Console.err.println(s"myId=$myId size=$width:$height")
+
+  val boardSym = (for (i <- 0 until height) yield readLine).map(_.toCharArray)
+  val board = boardSym.zipWithIndex.map(arrIndex => arrIndex._1.zipWithIndex.map(symIndex => new Square(symIndex._2, arrIndex._2, symIndex._1))).toArray
+
+  val legalSquares = board.flatten.filter(_.accessible)
+  val rand = new Random(System.currentTimeMillis)
+  val randSquare = legalSquares(rand.nextInt(legalSquares.length))
+
+  board.foreach(bl => {
+    bl.foreach(_.print)
+    Console.err.println})
+
+  // Write an action using println
+  // To debug: Console.err.println("Debug messages...")
+
+  println(s"${randSquare.getX} ${randSquare.getY}")
+  randSquare.accessible = false
+  val manager = new SquareManager(randSquare, board)
+
+//======================================================================================================================
   "A Player" should "run application" in {
     Player.main(Array.empty[String])
   }
 
+  "A SquareManager" should "find possible directions" in {
+    val sm = new SquareManager(/*randSquare*/board(13)(10), board)
+    val directions = sm.possibleDirection
+    Console.err.println(s"${directions.mkString(",")}")
+  }
 }
