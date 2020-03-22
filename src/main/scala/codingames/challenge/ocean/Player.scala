54da34a8c853813101e856378ff0142403bd9645
JolyRoger <mjelnr@gmail.com>
2020-03-22 15:44:31 +0100

    squares, manager

--- src/main/scala/codingames/challenge/ocean/Player.scala
+++ src/main/scala/codingames/challenge/ocean/Player.scala
@@ -1,25 +1,56 @@
 //package codingames.challenge.ocean
 
 import math._
+import scala.io.Source
 import scala.util._
 import scala.io.StdIn._
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
+
+class Square(x: Int, y: Int, sym: Char) {
+  val getX = x
+  val getY = y
+  val getSym = sym
+  def print = Console.err.print(s"$x:$y:$sym ")
+}
+
+class SquareManager(var myPosition: Square, board: Array[Array[Square]]) {
+  def possibleDirection = ee
+}
+
 object Player extends App {
+//------------------------------------------FILE ENTRY------------------------------------------------------------------
+/*
+  val filename = "ocean/ocean0.txt"
+  val bufferedSource = Source.fromFile(filename)
+  val data = bufferedSource.getLines
+
+  def readInt = if (data.hasNext) data.next.toInt else -1
+  def readLine = if (data.hasNext) data.next else "EOF"
+*/
+//----------------------------------------------------------------------------------------------------------------------
+
   val Array(width, height, myId) = (readLine split " ").map(_.toInt)
   Console.err.println(s"myId=$myId size=$width:$height")
-  for (i <- 0 until height) {
-    val line = readLine
-    Console.err.println(s"line=$line")
-  }
+
+  val boardSym = (for (i <- 0 until height) yield readLine).map(_.toCharArray)
+  val board = boardSym.zipWithIndex.map(arrIndex => arrIndex._1.zipWithIndex.map(symIndex => new Square(symIndex._2, arrIndex._2, symIndex._1))).toArray
+
+  val legalSquares = board.flatten.filter(_.getSym == '.')
+  val rand = new Random(System.currentTimeMillis)
+  val randSquare = legalSquares(rand.nextInt(legalSquares.length))
+
+
+
+
+//  board.foreach(bl => {
+//    bl.foreach(_.print)
+//    Console.err.println(s"\n")})
 
   // Write an action using println
   // To debug: Console.err.println("Debug messages...")
 
-  println("0 13")
+  println(s"${randSquare.getX} ${randSquare.getY}")
+  val manager = new SquareManager(randSquare, board)
 
   // game loop
   while (true) {
--- src/test/scala/codingame/challenge/ocean/PlayerTest.scala
+++ src/test/scala/codingame/challenge/ocean/PlayerTest.scala
@@ -1,7 +1,12 @@
 package codingame.challenge.ocean
 
+import codingames.challenge.ocean.Player
 import org.scalatest.FlatSpec
 
 class PlayerTest extends FlatSpec {
+
+  "A Player" should "run application" in {
+    Player.main(Array.empty[String])
+  }
 
 }
