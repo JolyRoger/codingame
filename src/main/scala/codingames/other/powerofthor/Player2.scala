506429c932255cb8d15422d3f3504cd8c3ada512
JolyRoger <mjelnr@gmail.com>
2023-01-06 00:17:14 +0100

    build fix

--- src/main/scala/codingames/other/powerofthor/Player2.scala
+++ src/main/scala/codingames/other/powerofthor/Player2.scala
@@ -1,4 +1,4 @@
 package codingames.other.powerofthor
 
 import scala.io.StdIn._
-object Player extends App {val Array(x,y,a,b)=(readLine split " ").map (_.toInt);var c=a;var d=b;var e="";var f="";while(true){readLine;if(c>x){e="W";c += 1;} else if(c<x){e="E";c -= 1;};if(d>y){f="N";d-=1}else if(d<y){f="S";d+=1;};println(s"$f$e");e="";f="";}}
+object Player2 extends App {val Array(x,y,a,b)=(readLine split " ").map (_.toInt);var c=a;var d=b;var e="";var f="";while(true){readLine;if(c>x){e="W";c += 1;} else if(c<x){e="E";c -= 1;};if(d>y){f="N";d-=1}else if(d<y){f="S";d+=1;};println(s"$f$e");e="";f="";}}
