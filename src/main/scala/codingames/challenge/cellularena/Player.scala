f7a3f21a15f8069cd8fb99062d90e0a6cc052003
JolyRoger <mjelnr@gmail.com>
2024-12-21 00:59:48 +0100

    harvest

--- src/main/scala/codingames/challenge/cellularena/Player.scala
+++ src/main/scala/codingames/challenge/cellularena/Player.scala
@@ -12,11 +12,11 @@ import scala.language.implicitConversions
  **/
 object Player extends App {
 //------------------------------------------FILE ENTRY------------------------------------------------------------------
- val filename = "resources/organisms/1.txt"
- val bufferedSource = Source.fromFile(filename)
- val data = bufferedSource.getLines
- def readInt = if (data.hasNext) data.next.toInt else { System.exit(0); -1 }
- def readLine = if (data.hasNext) data.next else { System.exit(0); "" }
+  val filename = "resources/organisms/1.txt"
+  val bufferedSource = Source.fromFile(filename)
+  val data = bufferedSource.getLines
+  def readInt = if (data.hasNext) data.next.toInt else { System.exit(0); -1 }
+  def readLine = if (data.hasNext) data.next else { System.exit(0); "" }
 //----------------------------------------------------------------------------------------------------------------------
 
   val debug = false
@@ -29,7 +29,7 @@ object Player extends App {
 
   object OrganType extends Enumeration {
     type OrganType = Value
-    val ROOT, BASIC = Value
+    val ROOT, BASIC, HARVESTER, TENTACLE, SPORER = Value
   }
 
   import OrganType._
@@ -40,92 +40,32 @@ object Player extends App {
     def y: Int
   }
   case class Wall(x: Int, y: Int) extends Tile
-  case class Organ(organId: Int, x: Int, y: Int, orgType: OrganType, orgDir: OrganDir, organParentId: Int, organRootId: Int) extends Tile {
+  case class Organ(organId: Int, x: Int, y: Int, orgType: OrganType, orgDir: OrganDir, organParentId: Int, organRootId: Int, age: Int) extends Tile {
     def grow(x: Int, y: Int) = s"GROW $organId $x $y BASIC"
   }
-  case class Protein(x: Int, y: Int) extends Tile
+  case class Protein(x: Int, y: Int, var harvested: Boolean = false) extends Tile
+  case class Harvest(candidate: Point, protein: Point, direction: OrganDir)
 
   var walls = List.empty[Wall]
   var myOrgan = List.empty[Organ]
   var oppOrgan = List.empty[Organ]
   var proteins = List.empty[Protein]
 
+  var myOrganMap = Map.empty[Int, Organ]
+  var oppOrganMap = Map.empty[Int, Organ]
+  var proteinMap = Map.empty[Point, Protein]
+
+  lazy val wallsSet = walls.map(wall => (wall.x, wall.y)).toSet
+  var myOrganSet = myOrgan.map(organ => (organ.x, organ.y)).toSet
+  var oppOrganSet = oppOrgan.map(organ => (organ.x, organ.y)).toSet
+  var proteinSet = proteins.map(protein => (protein.x, protein.y)).toSet
+
   var growCandidate: Organ = _
 
   // width: columns in the game grid
   // height: rows in the game grid
   val Array(width, height) = (readLine split " ").withFilter(_ != "").map (_.toInt)
-  Console.err.println(s"$width $height")
-
-
-
-
-// ----------------------------- GRAPH ---------------------------------------------------------------------------------
-  class Graph(N: Int) {
-    val adj = (for (_ <- 0 until N) yield List[Int]()).toArray
-
-    def n = N
-
-    def neighbourCandidates(x: Int, y: Int) = {
-      List((x, y + 1), (x, y - 1), (x + 1, y), (x - 1, y)).filter {
-        xy => xy._1 >= 0 &&
-          xy._1 < width &&
-          xy._2 >= 0 &&
-          xy._2 < height
-      }
-    }
-
-    def addEdge(v: Int, w: Int) {
-      adj(v) = w :: adj(v)
-      adj(w) = v :: adj(w)
-    }
-
-    def cutEdge(v: Int, w: Int): Unit = {
-      adj(v) = adj(v).filter(_ != w)
-      adj(w) = adj(w).filter(_ != v)
-    }
-
-    def bfs(s: Int) = {
-      val marked: Array[Boolean] = new Array[Boolean](n)
-      val edgeTo = Array.fill[Int](n)(Int.MaxValue)
-      val distTo = Array.fill[Int](n)(Int.MaxValue)
-      val q = mutable.Queue[Int]()
-      var i = 0
-
-      q.enqueue(s)
-      marked(s) = true
-      distTo(s) = 0
-      while (q.nonEmpty) {
-        val v = q.dequeue
-        i = i + 1
-        adj(v).filterNot(marked).foreach(
-          w => {
-            q.enqueue(w)
-            marked(w) = true
-            edgeTo(w) = v
-            distTo(w) = distTo(v) + 1
-          }
-        )
-      }
-      (edgeTo, distTo)
-    }
-
-    def path(from: Int, to: Int, edgeTo: Array[Int]): List[Int] = if (from == to) List(to) else List(from) ::: path(edgeTo(from), to, edgeTo)
-
-    def sortedPathAdj(path: List[Int]) =
-      (for (i <- adj.indices if path.contains(i)) yield i -> adj(i)).sortWith(_._2.size < _._2.size).toMap
-
-    def findNeighbour(node: Int, path: List[Int]): Int = {
-      for (i <- path.indices) {
-        if (path(i) == node) {
-          return if (i > 0) path(i - 1) else path(i + 1)
-        }
-      }
-      -1
-    }
-  }
-// ----------------------------- GRAPH ---------------------------------------------------------------------------------
-
+  if (debug) Console.err.println(s"$width $height")
 
 
 // ---------------------------------------------------------------------------------------------------------------------
@@ -140,22 +80,57 @@ object Player extends App {
 
   def euclidean(a: Point, b: Point): Double = hypot(b._1 - a._1, b._2 - a._2)
 
-  def clean = {
+  def adjWithDirection(organ: Point) = {
+    Set(
+      (organ._1, organ._2 + 1, S),
+      (organ._1, organ._2 - 1, N),
+      (organ._1 + 1, organ._2, E),
+      (organ._1 - 1, organ._2, W)
+    ).filter { xy =>
+      xy._1 >= 0 &&
+        xy._1 < width &&
+        xy._2 >= 0 &&
+        xy._2 < height &&
+        !wallsSet.contains((xy._1, xy._2))
+    }
+  }
+
+  def adj(organ: Point) = {
+    Set(
+      (organ._1, organ._2 + 1),
+      (organ._1, organ._2 - 1),
+      (organ._1 + 1, organ._2),
+      (organ._1 - 1, organ._2)
+    ).filter { xy =>
+      xy._1 >= 0 &&
+      xy._1 < width &&
+      xy._2 >= 0 &&
+      xy._2 < height &&
+      !wallsSet.contains(xy)
+    }
+  }
+
+  def clean {
     walls = List.empty
     myOrgan = List.empty
     oppOrgan = List.empty
     proteins = List.empty
   }
 
-// ---------------------------------------------------------------------------------------------------------------------
+  def harvest(organ: Organ) =
+    adj((organ.x, organ.y))
+      .filter(p => adj(p).exists(p => proteinSet.contains((p._1, p._2))))
+      .map(moveCandidate => adjWithDirection(moveCandidate)
+        .find(pd => proteinSet.contains((pd._1, pd._2)))
+        .map(proteinInfo => Harvest(moveCandidate, (proteinInfo._1, proteinInfo._2), proteinInfo._3)))
+      .headOption.flatten
 
-  val g = new Graph(width * height)
+// ---------------------------------------------------------------------------------------------------------------------
 
 // -------- game loop ------------------------------
   for (step <- LazyList.from(0).takeWhile(_ < 100)) {
     val entityCount = readLine.toInt
     if (debug) Console.err.println(s"$entityCount")
-    Console.err.println(s"step=$step")
     clean
 
     for(_ <- 0 until entityCount) {
@@ -174,23 +149,31 @@ object Player extends App {
       val organParentId = _organParentId.toInt
       val organRootId = _organRootId.toInt
 
-      _type match {
-        case "WALL" =>
-          val wall = Wall(x, y)
-          walls = wall :: walls
-        case "ROOT" =>
-          val organ = Organ(organId, x, y, ROOT, organDir, organParentId, organRootId)
-          if (owner == 1) myOrgan ::= organ else oppOrgan ::= organ
-        case "BASIC" =>
-          val organ = Organ(organId, x, y, BASIC, organDir, organParentId, organRootId)
-          if (owner == 1) myOrgan ::= organ  else oppOrgan ::= organ
-        case "A" =>
-          val protein = Protein(x, y)
-          proteins = protein :: proteins
+      if (owner == -1) {
+        _type match {
+          case "WALL" =>
+            val wall = Wall(x, y)
+            walls = wall :: walls
+          case "A" =>
+            val harvested = proteinMap.get((x, y)).exists(_.harvested)
+            val protein = Protein(x, y, harvested)
+            proteins = protein :: proteins
+        }
+      } else {
+        val allOrgans = myOrganMap ++ oppOrganMap
+        val organAge = allOrgans.get(organId).map(_.age).getOrElse(step)
+        val organ = Organ(organId, x, y, OrganType.withName(_type), organDir, organParentId, organRootId, organAge)
+        if (owner == 1) myOrgan ::= organ else oppOrgan ::= organ
       }
     }
+
+    myOrganSet = myOrgan.map(organ => (organ.x, organ.y)).toSet
+    oppOrganSet = oppOrgan.map(organ => (organ.x, organ.y)).toSet
+    proteinMap = proteins.map(protein => ((protein.x, protein.y), protein)).toMap
+    proteinSet = proteinMap.keySet
+
     // myD: your protein stock
-    val Array(myA, myB, myC, myD) = (readLine split " ").filter(_ != "").map (_.toInt)
+    val Array(myA, myB, myC, myD) = (readLine split " ").withFilter(_ != "").map (_.toInt)
     if (debug) Console.err.println(s"$myA $myB $myC $myD")
 
     // oppD: opponent's protein stock
@@ -203,13 +186,23 @@ object Player extends App {
     for(_ <- 0 until requiredActionsCount) {
       // Write an action using println
       // To debug: Console.err.println("Debug messages...")
-      growCandidate = myOrgan.reverse.head
-      val closestProtein = proteins.minByOption(protein => euclidean((protein.x, protein.y), (growCandidate.x, growCandidate.y)))
-      println {
-        s"${closestProtein
+      growCandidate = myOrgan.maxBy(_.age)
+
+      val command = harvest(growCandidate)
+        .filter(_ => myC > 0 && myD > 0)
+        .map(hrv => {
+          val protein = proteinMap(hrv.protein)
+          protein.harvested = true
+          s"GROW ${growCandidate.organId} ${hrv.candidate._1} ${hrv.candidate._2} HARVESTER ${hrv.direction}"
+        })
+        .getOrElse {
+          val closestProtein = proteins.minByOption(protein => euclidean((protein.x, protein.y), (growCandidate.x, growCandidate.y)))
+          s"${closestProtein
             .map(protein => s"GROW ${growCandidate.organId} ${protein.x} ${protein.y} BASIC")
-            .getOrElse("WAIT")}"
-      }
+            .getOrElse(s"GROW ${growCandidate.organId} ${oppOrgan.head.x} ${oppOrgan.head.y} BASIC")}"
+        }
+
+      println(command)
 
     }
   }
